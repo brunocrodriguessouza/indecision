0d97c25c7e7d686d498913c361e816dbf763ded1
Bruno Souza <brunoprogramadorjava@gmail.com>
2018-11-28 07:52:50 -0200

    [section3] 9. Setting up  Babel

---
+++ src/App.js
@@ -0,0 +1,7 @@
+console.log('App.js is running!');
+
+// JSX - JavaScript XML
+var template = <h1>Indecision App</h1>;
+var appRoot = document.getElementById('App');
+
+ReactDOM.render(template, appRoot);
