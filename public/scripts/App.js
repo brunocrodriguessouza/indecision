091075edaf48e4f064d87cebe0348fe3909f4618
Bruno Souza <brunoprogramadorjava@gmail.com>
2018-12-01 09:16:37 -0200

    [section3] 16. Events and Attributes

--- public/scripts/App.js
+++ public/scripts/App.js
@@ -2,80 +2,81 @@
 
 console.log('App.js is running!');
 
-// create app object title/subtitle
-// user title/subtitle in the template
-// render template 
-
-// JSX - JavaScript XML
-
 var app = {
-    title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer',
-    options: ['One', 'Two']
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
 };
-
 var template = React.createElement(
-    'div',
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'Here are your options' : 'No options'
+  ),
+  React.createElement(
+    'ol',
     null,
     React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        app.subtitle
+      'li',
+      null,
+      'Item one'
     ),
     React.createElement(
-        'p',
-        null,
-        app.options.length > 0 ? 'Here are your options' : 'No options'
-    ),
-    app.options.length > 0 && React.createElement(
-        'ol',
-        null,
-        React.createElement(
-            'li',
-            null,
-            'Item one'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'Item two'
-        )
+      'li',
+      null,
+      'Item two'
     )
+  )
 );
 
-var user = {
-    name: 'Andrew',
-    age: 26,
-    location: 'Philadelphia'
+var count = 0;
+var addOne = function addOne() {
+  console.log('addOne');
 };
-
+var minusOne = function minusOne() {
+  console.log('minusOne');
+};
+var reset = function reset() {
+  console.log('reset');
+};
 var templateTwo = React.createElement(
-    'div',
+  'div',
+  null,
+  React.createElement(
+    'h1',
     null,
-    React.createElement(
-        'h1',
-        null,
-        user.name
-    ),
-    user.age > 18 && React.createElement(
-        'p',
-        null,
-        'Age: ',
-        user.age
-    ),
-    React.createElement(
-        'p',
-        null,
-        'Location: ',
-        user.location
-    )
+    'Count: ',
+    count
+  ),
+  React.createElement(
+    'button',
+    { onClick: addOne },
+    '+1'
+  ),
+  React.createElement(
+    'button',
+    { onClick: minusOne },
+    '-1'
+  ),
+  React.createElement(
+    'button',
+    { onClick: reset },
+    'reset'
+  )
 );
 
-var appRoot = document.getElementById('App');
+var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/App.js
+++ src/App.js
@@ -1,44 +1,41 @@
 console.log('App.js is running!');
 
-// create app object title/subtitle
-// user title/subtitle in the template
-// render template 
-
-// JSX - JavaScript XML
-
-var app = {
-    title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer',
-    options: ['One', 'Two']
+const app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
 };
-
-var template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-        { app.options.length > 0 && 
-        <ol>
-            <li>Item one</li>
-            <li>Item two</li>
-        </ol>}
-    </div>
+const template = (
+  <div>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
 );
 
-var user = {
-    name: 'Andrew',
-    age: 26,
-    location: 'Philadelphia'
+let count = 0;
+const addOne = () => {
+  console.log('addOne');
 };
+const minusOne = () => {
+  console.log('minusOne');
+};
+const reset = () => {
+  console.log('reset');
+};
+const templateTwo = (
+  <div>
+    <h1>Count: {count}</h1>
+    <button onClick={addOne}>+1</button>
+    <button onClick={minusOne}>-1</button>
+    <button onClick={reset}>reset</button>
+  </div>
+);
 
-var templateTwo = (
-    <div>
-        <h1>{user.name}</h1>
-        {(user.age>18) && <p>Age: {user.age}</p>}
-        <p>Location: {user.location}</p>
-    </div>
-)
+const appRoot = document.getElementById('app');
 
-var appRoot = document.getElementById('App');
-
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
