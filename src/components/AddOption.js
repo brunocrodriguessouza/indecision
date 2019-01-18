fa4214cd44a61ec2f81645e4c43690ce1a584781
Bruno Souza <brunoprogramadorjava@gmail.com>
2019-01-17 23:29:07 -0200

    [section6] 56. Source Maps with Webpack

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -11,8 +11,6 @@ export default class AddOption extends React.Component {
   handleAddOption(e) {
     e.preventDefault();
 
-    console.log("Adding option");
-
     const option = e.target.elements.option.value.trim();
     const error = this.props.handleAddOption(option);
 
