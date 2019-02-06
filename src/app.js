c3bf2b57e2d2766af2c70e57fc87aa3c2472c9de
Bruno Souza <brunoprogramadorjava@gmail.com>
2019-02-06 08:39:42 -0200

    [section8] 66. Reset That 0status

--- src/app.js
+++ src/app.js
@@ -2,22 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 import './styles/styles.scss';
+import 'normalize.css/normalize.css';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
-
-// import validator from 'validator';
-
-// console.log(validator.isEmail('bruno@gmail.com'));
-
-// import './utils.js';
-// import subtract, { square, add } from './utils.js';
-
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 20));
-
-// import isSenior, { isAdult, canDrink } from './person.js';
-// console.log(isAdult(18));
-// console.log(canDrink(20));
-// console.log(isSenior(65));
